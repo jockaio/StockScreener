9a88946846cc3d32a3925cd8aa2a1c9fee038098
Joakim <joakimhellerstrom@hotmail.com>
2016-10-06 20:12:48 +0200

    connectionString and upprensning done.

--- StockScreener2/StockScreener2/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace StockScreener2.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- StockScreener2/StockScreener2/Service/JsonConverter.cs
+++ StockScreener2/StockScreener2/Service/JsonConverter.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Runtime.Serialization.Json;
 using System.Text;
-using System.Web;
 
 namespace StockScreener2.Service
 {
